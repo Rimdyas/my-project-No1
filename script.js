9eacf8b7f5b65fea2581264c4df31ff3636f1238
VikaKhaustova <haustovavika@gmail.com>
2024-09-20 22:51:24 +0300

    create registration.html

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// Отримуємо кнопку "Кошик"
+const cartBtn = document.getElementById('cartBtn');
 
+// Навішуємо обробник подій на клік кнопки "Кошик"
+cartBtn.addEventListener("click", function () {
+    // Переходимо на сторінку кошика
+    window.location.assign('registration.html');
+});
